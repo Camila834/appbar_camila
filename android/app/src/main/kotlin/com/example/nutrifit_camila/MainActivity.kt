dfb9b616a09ebc7ec72ce6660f5fa120cd698a60
senai <evelyn.quispe@aluno.senai.br>
2026-08-27 11:08:50 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/nutrifit_camila/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nutrifit_camila
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/styles/drawer_styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class DrawerStyles {
+  static const Color corCabecalho = Colors.blue;
+  static const textoItem = TextStyle(fontSize: 16);
+}
